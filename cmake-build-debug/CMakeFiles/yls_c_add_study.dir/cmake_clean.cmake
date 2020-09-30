f49a7cb3f4beef769b210f064bade6230f3939c8
yls <2689623200@qq.com>
2020-09-30 10:45:12 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/yls_c_add_study.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yls_c_add_study.dir/main.cpp.obj"
+  "libyls_c_add_study.dll.a"
+  "yls_c_add_study.exe"
+  "yls_c_add_study.exe.manifest"
+  "yls_c_add_study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yls_c_add_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
